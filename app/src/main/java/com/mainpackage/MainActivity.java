0ca7408fea6b6e399cd928232bca4b535e4cdde9
Gašper Viher <gasper.viher@gmail.com>
2022-05-27 14:26:20 +0200

    Test commit

--- app/src/main/java/com/mainpackage/MainActivity.java
+++ app/src/main/java/com/mainpackage/MainActivity.java
@@ -41,7 +41,7 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
-
+//Test comment
 public class MainActivity extends AppCompatActivity {
     EditText username,password;
     TextView onUsername, onPassword;
